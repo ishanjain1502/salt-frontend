1bb1975adef7fea15e0a4b4ffa73798f9332ac92
Ishan <ishanjain1502@gmail.com>
2022-08-26 21:14:46 +0530

    changes added

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,11 +2,11 @@ import React , {useState, useEffect} from 'react';
 import jwt from 'jsonwebtoken'
 import styles from '../styles/Home.module.css'
 
-type Props = {}
 
-const dashboard = (props: Props) => {
+
+const dashboard = () => {
     // const name = window.localStorage.getItem('username')
-    const [name, setName] = useState("")
+    const [usename, setName] = useState("")
 
 
     useEffect(() => {
@@ -28,8 +28,9 @@ const dashboard = (props: Props) => {
 	}
 
   return (
-    <div>Welcome <p className={styles.title} >{name}</p> </div>
+    <div>Welcome <p className={styles.title} >{usename}</p> </div>
   )
 }
 
 export default dashboard
+
